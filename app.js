e204054b8328682d18a3f7eca8989a67ea9c143f
Ubuntu <ubuntu@ip-172-31-42-211.eu-north-1.compute.internal>
2026-09-24 10:35:06 +0000

    Lastest_code

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send(`<h1>CI/CD Pipeline Automation!</h1><p>Deployed via Jenkins + Docker</p><p>Build: ${process.env.BUILD_NUMBER || 'local'}</p>`);
+  res.send(`<h1>Devops CI/CD Pipeline Automation!</h1><p>Deployed via Jenkins + Docker</p><p>Build: ${process.env.BUILD_NUMBER || 'local'}</p>`);
 });
 
 app.get('/health', (req, res) => {
